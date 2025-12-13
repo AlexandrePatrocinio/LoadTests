efcb4e7d6d2912f09436c417f139b2f3aa6fc57f
Alexandre Patrocinio <patrocinio.alexandre@hotmail.com>
2025-12-13 17:20:10 -0300

    script modification to docker compose plugin

--- reset-k6-db.sh
+++ reset-k6-db.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "🔄 Limpando banco k6 no InfluxDB..."
 
-docker-compose exec influxdb influx -execute "DROP DATABASE k6" || true
-docker-compose exec influxdb influx -execute "CREATE DATABASE k6"
+docker compose exec influxdb influx -execute "DROP DATABASE k6" || true
+docker compose exec influxdb influx -execute "CREATE DATABASE k6"
 
 echo "✅ Banco k6 recriado com sucesso!"
